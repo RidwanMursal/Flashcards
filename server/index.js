d33b32be316076f35798a976d3cf2f03c7ddbea5
ridwanmursal <ridwan09@hotmail.com>
2023-01-21 13:51:58 -0500

    changed cors origin

--- server/index.js
+++ server/index.js
@@ -1,37 +1,51 @@
-const express = require("express")
-const bodyParser = require("body-parser") 
-const cardsRouter = require("./routes/cards.js")
-const decksRouter = require("./routes/decks.js")
-const classesRouter = require("./routes/classes.js")
-const usersRouter = require("./routes/users.js")
-const authRouter = require("./routes/auth.js")
-const imagesRouter = require("./routes/images")
+const express = require("express");
+const bodyParser = require("body-parser");
+const cardsRouter = require("./routes/cards.js");
+const decksRouter = require("./routes/decks.js");
+const classesRouter = require("./routes/classes.js");
+const usersRouter = require("./routes/users.js");
+const authRouter = require("./routes/auth.js");
+const imagesRouter = require("./routes/images");
 
-//middlewares 
-const {authenticateToken} = require("./middlewares/authentication.js")
-const cors = require("cors")
+//middlewares
+const { authenticateToken } = require("./middlewares/authentication.js");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-require("dotenv").config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({limit: "5mb"}))
+app.use(bodyParser.json({ limit: "5mb" }));
 app.use(cookieParser());
-app.use(cors({origin: "*", methods: ["get", "GET", "post", "POST", "patch", "PATCH", "delete", "DELETE"]}))
+app.use(
+  cors({
+    origin: "https://flashcard-client.vercel.app",
+    methods: [
+      "get",
+      "GET",
+      "post",
+      "POST",
+      "patch",
+      "PATCH",
+      "delete",
+      "DELETE",
+    ],
+  })
+);
 
+// static photos
+app.use("/static", express.static("./images"));
+// routes
+app.use("/auth", authRouter);
+app.use(authenticateToken);
+app.use("/cards", cardsRouter);
+app.use("/decks", decksRouter);
+app.use("/classes", classesRouter);
+app.use("/users", usersRouter);
+app.use("/images", imagesRouter);
 
-// static photos 
-app.use('/static', express.static('./images'))
-// routes 
-app.use("/auth", authRouter)
-app.use(authenticateToken)
-app.use("/cards", cardsRouter)
-app.use("/decks", decksRouter )
-app.use("/classes", classesRouter )
-app.use("/users", usersRouter)
-app.use("/images", imagesRouter)
+app.get("/", (req, res) => res.send("hello"));
 
-app.get("/", (req, res) => res.send("hello"))
-
-
-app.listen(process.env.PORT, () => console.log("i am listening on port", process.env.PORT))
+app.listen(process.env.PORT, () =>
+  console.log("i am listening on port", process.env.PORT)
+);
