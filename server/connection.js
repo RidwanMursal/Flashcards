1c9622343bbac24b3af241e3917633bc59bea024
ridwanmursal <ridwan09@hotmail.com>
2023-01-21 01:33:52 -0500

    assigned new connection for db

--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,20 @@
-const Pool = require("pg").Pool
+const Pool = require("pg").Pool;
+require("dotenv").config();
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "new_password",
+//     host: "localhost",
+//     port: 5432,
+//     database: "flashcards"
+// })
 
 const pool = new Pool({
-    user: "postgres", 
-    password: "new_password", 
-    host: "localhost", 
-    port: 5432, 
-    database: "flashcards"
-})
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+});
 
-module.exports = pool
+module.exports = pool;
